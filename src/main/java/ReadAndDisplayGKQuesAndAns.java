0726d4df7ef2daa0249208ab77fbaa0defc99c65
abid-novel <abidnovel03@gmail.com>
2022-01-16 00:55:39 +0600

    Write function to get the correct answer

--- src/main/java/ReadAndDisplayGKQuesAndAns.java
+++ src/main/java/ReadAndDisplayGKQuesAndAns.java
@@ -3,10 +3,13 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class ReadAndDisplayGKQuesAndAns {
+
+    //function for read the question and display
     public static void readAndDisplayQuesFile(int pos) throws IOException, ParseException {
         String fileName = "./src/main/resources/GeneralKnowledgeQuestionAndAnswer.json";
         JSONParser jsonParser = new JSONParser();
@@ -30,5 +33,18 @@ public class ReadAndDisplayGKQuesAndAns {
 
     }
 
+    //function for read the correct answer only
+    public static String readAnswerOnly(int pos) throws IOException, ParseException {
+        String fileName = "./src/main/resources/GeneralKnowledgeQuestionAndAnswer.json";
+        JSONParser jsonParser = new JSONParser();
+        Object obj = jsonParser.parse(new FileReader(fileName));
+        JSONArray jsonArray = (JSONArray) obj;
 
+        //Get the correct answer
+        JSONObject ansObject = (JSONObject) jsonArray.get(pos);
+        String correctAnswer = (String) ansObject.get("Answer");
+        return correctAnswer;
+
+
+    }
 }
