5b1a3f2ebd247fbf32aa9d5a32dbe3fc1c8ca1d7
abid-novel <abidnovel03@gmail.com>
2022-01-15 23:30:12 +0600

    Remove comment

--- src/main/java/RunTheQuiz.java
+++ src/main/java/RunTheQuiz.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class RunTheQuiz {
     public static void main(String[] args) throws IOException, ParseException {
-        //WriteJSONArrayList.writeGeneralKnowledgeQues();
+
 
     }
 }
